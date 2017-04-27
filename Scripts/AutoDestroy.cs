664d633b3fe4086933f827fcfb0afd0c5e43e6fe
gnatal95 <guilherme.natal47@gmail.com>
2017-04-27 20:26:30 -0300

    alguns arquivos do tg

---
+++ Scripts/AutoDestroy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDestroy : MonoBehaviour {
+    private float crono;
+    public float timeNow;
+    public float ExTime;
+	// Use this for initialization
+
+	void Start () {
+        crono = Time.fixedTime;
+        ExTime = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeNow = Time.fixedTime;
+		if (timeNow > crono + ExTime) {
+            Destroy(gameObject);
+        }
+	}
+}
